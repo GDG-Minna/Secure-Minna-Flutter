30e0915e2ca8a71dd3ba8a0e16acf3263d384f9b
Umar Saidu Auna <umarmanofpeace@email.com>
2022-09-17 20:56:45 +0100

    feat: 🎸 implement federal road safety screen

--- lib/screens/federal_road_safety_screens/federal_road_safety_detail_page.dart
+++ lib/screens/federal_road_safety_screens/federal_road_safety_detail_page.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:secure_minna/models/SecurityAgenciesModel.dart';
+import 'package:secure_minna/screens/federal_road_safety_screens/federal_road_safety_tab_one_page.dart';
+import 'package:secure_minna/screens/federal_road_safety_screens/federal_road_safety_tab_two_page.dart';
 
 class FederalRoadSafetyDetailPage extends StatelessWidget {
 
@@ -10,15 +12,36 @@ class FederalRoadSafetyDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         appBar: AppBar(
-          title: Text(
-              "${items.title}",
+          title: Text("${items.title}",
               style: TextStyle(
-                fontWeight: FontWeight.normal,  fontFamily: 'Poppins',)
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Poppins',
+              )),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                /*icon: Icon(Icons.contacts),*/
+                text: "Emergency Call",
+              ),
+              Tab(
+                /*icon: Icon(Icons.contacts),*/
+                text: "Map Location",
+              )
+            ],
           ),
         ),
-        body: Center(child: Text("${items.email}")));
+        body: TabBarView(
+          children: [
+            FederalRoadSafetyTabOnePage(items: items,),
+            FederalRoadSafetyTabTwoPage(items: items,)
+          ],
+        ),
+      ),
+    );
   }
 
 }
