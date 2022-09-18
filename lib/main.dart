30d2a2f9cbaeff60ebeb934bc5adc3b15a7c2fdf
Umar Saidu Auna <umarmanofpeace@email.com>
2022-09-18 12:36:53 +0100

    refactor: 💡 main.dart class

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:secure_minna/screens/home_screens/home_page.dart';
 import 'package:secure_minna/screens/onboarding_screens/splash_screen_page.dart';
 
-import 'onboarding_screens/onboarding_page.dart';
+import 'screens/onboarding_screens/onboarding_page.dart';
 
 int initScreen;
 void main() async  {
