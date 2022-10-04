ea8a1144ba8d16343ee6bb5ecae1872dc75ad480
Umar Saidu Auna <umarmanofpeace@email.com>
2022-10-04 13:18:02 +0100

    feat: 🎸 create custom class for colors, modify police force

---
+++ lib/components/secure_minna_colors.dart
@@ -0,0 +1,12 @@
+
+import 'dart:ui';
+
+class SecureMinnaColors{
+  //SecureMinnaColors.home
+  static const Color primary = const Color(0xFF55A3DA);
+  static const Color secondary = const Color(0xFFA1DAF4);
+  static const Color black = const Color(0xFF000000);
+  static const Color lightWhite = const Color(0xFF7A7E80);
+  static const Color white = const Color(0xFFFFFFFF);
+  static const Color lightBlack = const Color(0xFF474747);
+}
--- lib/screens/home_screens/privacy_policy_page.dart
+++ lib/screens/home_screens/privacy_policy_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../components/navigation_drawer.dart';
 
 class PrivacyPolicyPage extends StatelessWidget {
